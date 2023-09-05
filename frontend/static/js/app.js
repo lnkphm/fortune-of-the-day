c2d0903ca2dc8b18f6ff31d76b1327f1ff6f5a82
Linh Pham <lnkphm@gmail.com>
2023-09-05 15:15:45 +0700

    feat: add frontend

---
+++ frontend/static/js/app.js
@@ -0,0 +1,25 @@
+function getRandom(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+async function getFortune(id) {
+    const response = await fetch(`http://127.0.0.1:5000/fortunes/${id}`);
+    const fortune = await response.json();
+    return fortune
+}
+
+async function loadFortune() {
+    rand = getRandom(0, 10);
+    fortune = await getFortune(rand);
+    showFortune(fortune)
+}
+
+function showFortune(fortune) {
+    fortuneElem = document.getElementById("fortune-text");
+    fortuneElem.textContent = fortune.name;
+}
+
+
+window.addEventListener("load", loadFortune)
